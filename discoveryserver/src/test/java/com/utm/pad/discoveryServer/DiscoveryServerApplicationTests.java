7be61a0e0530e9eb726147ec6332ebae5db0461b
maximums <cdodi907@gmail.com>
2021-10-12 16:20:14 +0300

    init system

---
+++ discoveryserver/src/test/java/com/utm/pad/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utm.pad.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
